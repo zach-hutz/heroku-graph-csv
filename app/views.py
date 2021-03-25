80d034e3ee461206443af47caf2e4dfa2b8df246
zach-hutz <78053978+zach-hutz@users.noreply.github.com>
2021-03-25 18:49:26 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -216,8 +216,9 @@ def dashboard():
         print(user_id_list_dump)
         str_id = str(current_user.get_id())
         directory = os.path.join("/app/app/static/user_data/",str_id)
-        fix_directory = os.path.join("/app/app/static/user_data/",str_id)
-        fix_directory = os.path.join(fix_directory,"/")
+        fix_directory = "/app/app/static/user_data/",str_id
+        fix_directory = fix_directory+"/"
+        print(fix_directory)
         print(os.path.abspath(os.getcwd()))
 
         absolute_path = os.path.abspath(__file__)
